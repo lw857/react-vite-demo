5852f3c6da9c3181f971d12d54a021e274f73042
lw0000 <8765213+lw0000@user.noreply.gitee.com>
2024-09-13 01:10:39 +0800

    add

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-vite-demo">
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
