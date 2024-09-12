c17a817380f5c28634d5554a2000cb4128a6b82b
lw0000 <8765213+lw0000@user.noreply.gitee.com>
2024-09-12 22:47:19 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR11
+          Edit <code>src/App.tsx</code> and save to test HMR111111
         </p>
       </div>
       <p className="read-the-docs">
