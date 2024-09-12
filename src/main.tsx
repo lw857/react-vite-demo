541ce1a81632fb86e5c292e6173a063c7098692c
lw0000 <8765213+lw0000@user.noreply.gitee.com>
2024-09-13 01:45:32 +0800

    1123

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter basename="/react-vite-demo/">
+  <BrowserRouter basename="/react-vite-demo/">
+    <StrictMode>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </StrictMode>
+  </BrowserRouter>
 );
